21d86e13efffc94e3aca9adbc03104facbbb9c2f
蔡丽园 <18822321536@163.com>
2015-05-29 19:21:42 +0800

    test

--- src/main/java/KafkaTopology.java
+++ src/main/java/KafkaTopology.java
@@ -1,28 +1,22 @@
 import backtype.storm.Config;
-import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.utils.Utils;
-
-import java.util.HashMap;
-import java.util.Map;
 /**
  * Created by CLY on 2015/5/28.
  */
 public class KafkaTopology {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
         TopologyBuilder builder = new TopologyBuilder();
 
         builder.setSpout("spout", new KafkaSpout(""), 1);
         builder.setBolt("bolt", new DataBolt()).shuffleGrouping("spout");
 
-        Map conf = new HashMap();
-        conf.put(Config.TOPOLOGY_WORKERS, 1);
-        conf.put(Config.TOPOLOGY_DEBUG, true);
+        Config config = new Config();
+        config.setNumWorkers(4);
+        config.setMaxSpoutPending(1000);
 
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("my-flume-kafka-storm-topology-integration", conf, builder.createTopology());
-
-        Utils.sleep(1000*60*5); // local cluster test ...
-        cluster.shutdown();
+        StormSubmitter.submitTopology("my-flume-kafka-storm-topology-integration", config, builder.createTopology());
     }
 }
